644dfaa5927ea356879aebfbfe72f9dba4eaff89
Alexandr Chussovitin <alexandr.chussovitin@gmail.com>
2011-05-23 17:38:01 +0200

    Version 0.11

--- src/de/hofuniversity/sta/ts/db/JDBCTest.java
+++ src/de/hofuniversity/sta/ts/db/JDBCTest.java
@@ -6,6 +6,7 @@ import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Date;
 
 public class JDBCTest {
 	private static final String URL = "jdbc:mysql://localhost/studienArbeitDB";
@@ -19,10 +20,23 @@ public class JDBCTest {
 		Statement stmt = con.createStatement();
 		ResultSet res = stmt.executeQuery("select * from users");
 		while(res.next()){
-			int id = res.getInt(1);
-			String name = res.getString(2);
-			String hgid = res.getString(3);
-			System.out.printf("%d\t%s\t%d%n", id, name, hgid);
+			int userID = res.getInt(1);
+			String username = res.getString(2);
+			String passwort = res.getString(3);
+			String firstname = res.getString(4);
+			String lastname = res.getString(5);
+			Date dateofBirth = res.getDate(6);
+			Date regDate = res.getDate(7);
+			String city = res.getString(8);
+			String street = res.getString(9);
+			String housenumber = res.getString(10);
+			String postCode = res.getString(11);
+			String country = res.getString(12);
+			System.out.printf("%d\n%s\n%s\n",userID, username, passwort);
+			System.out.printf("%s\n%s\n%s\n",firstname, lastname, dateofBirth);
+			System.out.printf("%s\n%s\n%s\n",regDate, city, street);
+			System.out.printf("%s\n%s\n%s\n",housenumber, postCode, country);
+			
 		}
 		con.close();
 	}
